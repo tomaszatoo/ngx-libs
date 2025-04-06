46f91e555d46c588739685c731797cf13722380e
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-04-06 16:25:07 +0200

    add ngx-timer example

--- projects/tomaszatoo/examples/src/app/app.routes.ts
+++ projects/tomaszatoo/examples/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
-import { NgxWpApiComponent } from './example/component/ngx-wp-api.component';
+import { NgxWpApiComponent } from './components/ngx-wp-api/ngx-wp-api.component';
+import { NgxTimerComponent } from './components/ngx-timer/ngx-timer.component';
 
 export const routes: Routes = [
     { path: 'ngx-wp-api', component: NgxWpApiComponent },
-
+    { path: 'ngx-timer', component: NgxTimerComponent }
 ];
