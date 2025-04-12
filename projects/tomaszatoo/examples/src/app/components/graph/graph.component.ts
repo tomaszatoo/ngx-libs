2f9ee7f98f56c80359cb02f3215391e0a206cc6a
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-04-12 19:32:28 +0200

    update packages + update graph example + implement rename graph-viewer

--- projects/tomaszatoo/examples/src/app/components/graph/graph.component.ts
+++ projects/tomaszatoo/examples/src/app/components/graph/graph.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // @tomaszatoo/graph-visualizer
-import { GraphEngineService, GraphViewerComponent, LayoutSettings } from '@tomaszatoo/graph-visualizer';
+import { GraphViewerComponent, GraphLayoutSettings, GraphVisualizerOptions } from '@tomaszatoo/graph-viewer';
 import { Graphics } from 'pixi.js';
 
 @Component({
@@ -14,44 +14,26 @@ export class GraphComponent implements OnInit {
   // Define renderers as class properties to bind via Input
   graphData = {
     nodes: [
-      { id: 'a', attributes: { label: 'Node A', color: 0xff0000, stroke: 0x00ff00, x: 1, y: 1 } },
-      { id: 'b', attributes: { label: 'Node B', color: 0x00ff00, stroke: 0x00ff00, x: 1, y: 1 } },
-      { id: 'c', attributes: { label: 'Node C', color: 0x0000ff, stroke: 0x00ff00, x: 1, y: 1 } },
-      { id: 'd', attributes: { label: 'Node D', color: 0xffff00, stroke: 0x00ff00, x: 1, y: 1 } }
+      { id: 'e', attributes: { label: 'Node E', x: 1, y: 1 } },
+      { id: 'f', attributes: { label: 'Node F', x: 1, y: 1 } },
+      { id: 'g', attributes: { label: 'Node G', x: 1, y: 1 } },
+      { id: 'h', attributes: { label: 'Node H', x: 1, y: 1 } }
     ],
     edges: [
-      { source: 'a', target: 'b', attributes: { label: "follow", width: 2, color: 0xff0000 } },
-      { source: 'a', target: 'c', attributes: { label: "follow", width: 3, color: 0x00ff00 } },
-      { source: 'b', target: 'c', attributes: { label: "follow", width: 0.5 }},
-      { source: 'b', target: 'a', attributes: { label: "follow", width: 0.5 }},
-      { source: 'd', target: 'a', attributes: { label: "follow", width: 0.5 }},
-      { source: 'd', target: 'b', attributes: { label: "follow", color: 0x000000 }}
-    ],
-  };
-  graphData2 = {
-    nodes: [
-      { id: 'e', attributes: { label: 'Node E', color: 0x000000, stroke: 0xffffff, x: 1, y: 1 } },
-      { id: 'f', attributes: { label: 'Node F', color: 0x000000, stroke: 0xffffff, x: 1, y: 1 } },
-      { id: 'g', attributes: { label: 'Node G', color: 0x000000, stroke: 0xffffff, x: 1, y: 1 } },
-      { id: 'h', attributes: { label: 'Node H', color: 0x000000, stroke: 0xffffff, x: 1, y: 1 } }
-    ],
-    edges: [
-      { source: 'f', target: 'e', attributes: { label: "follow", width: 2, color: 0xffffff }},
+      { source: 'f', target: 'e', attributes: { label: "follow", width: 2, color: 0x30D973 }},
       /* { source: 'f', target: 'e', attributes: { label: "follow", width: 2, color: 0xff0000 }}, */ // not works with multi: false (solve by attributes (add to already existing))
-      { source: 'h', target: 'e', attributes: { label: "follow", width: 2, color: 0xffffff }},
-      { source: 'h', target: 'h', attributes: { label: "follow", width: 2, color: 0xffffff }},
-      { source: 'h', target: 'f', attributes: { label: "follow", width: 2, color: 0xffffff }}
+      { source: 'h', target: 'e', attributes: { label: "follow", width: 2, color: 0x30D973 }},
+      { source: 'h', target: 'h', attributes: { label: "follow", width: 2, color: 0x30D973 }},
+      { source: 'h', target: 'f', attributes: { label: "follow", width: 2, color: 0x30D973 }}
     ],
   };
   renderNode = this.createNodeRenderer();
   renderEdge = this.createEdgeRenderer();
 
   fullscreen: boolean = false;
-  selectedNode: string = '';
+  selectNode: string = '';
 
-  // private layout: any;
-
-  layoutSettings: LayoutSettings = {
+  layoutSettings: GraphLayoutSettings = {
     adjustSizes: true,
     barnesHutOptimize: false,
     barnesHutTheta: 0.5,
@@ -61,20 +43,7 @@ export class GraphComponent implements OnInit {
     outboundAttractionDistribution: true,
     scalingRatio: 50,
     slowDown: 2,
-    strongGravityMode: false
-  }
-
-  layoutSettings2: LayoutSettings = {
-    adjustSizes: true,
-    barnesHutOptimize: false,
-    barnesHutTheta: 0.5,
-    edgeWeightInfluence: 1,
-    gravity: .1,
-    linLogMode: true,
-    outboundAttractionDistribution: true,
-    scalingRatio: 50,
-    slowDown: 2,
-    strongGravityMode: true
+    strongGravityMode: true // very effective
   }
 
   constructor(
@@ -88,17 +57,15 @@ export class GraphComponent implements OnInit {
       }
     });
 
-    setTimeout(() => {
-      this.selectedNode = 'e';
-    }, 5000);
-    setTimeout(() => {
-      this.selectedNode = 'g';
-    }, 10000);
-    setTimeout(() => {
-      this.selectedNode = 'h';
-    }, 15000);
-    // this.graphEngine.loadGraph(graphData);
-    // this.layout = this.graphEngine.getLayout();
+    // setTimeout(() => {
+    //   this.selectedNode = 'e';
+    // }, 5000);
+    // setTimeout(() => {
+    //   this.selectedNode = 'g';
+    // }, 10000);
+    // setTimeout(() => {
+    //   this.selectedNode = 'h';
+    // }, 15000);
   }
 
   createNodeRenderer() {
@@ -130,7 +97,6 @@ export class GraphComponent implements OnInit {
   }
 
   log(key: any, value: any): void {
-    console.log('log');
     console.log(key, value);
   }
 
